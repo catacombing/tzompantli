14713e3d32fdeb31a94da3e72ddc9c2df6f43671
linkmauve <linkmauve@linkmauve.fr>
2023-07-10 23:02:33 +0200

    Split SVG rendering in two steps
    resvg has the interesting property that it does half of the work during
    Tree building, to then make rendering as fast as possible.
    
    For now we only use this to avoid re-parsing the poweroff and reboot
    icons, but in the future we can use this also on other icons.
    
    In the future we will also do the parsing early in a thread pool, and
    only do the rendering once we know the size at which we will get
    displayed.

--- src/svg.rs
+++ src/svg.rs
@@ -1,5 +1,6 @@
 //! SVG rasterization.
 
+use core::fmt;
 use std::path::Path;
 use std::{fs, io};
 
@@ -26,34 +27,58 @@ impl From<io::Error> for Error {
     }
 }
 
-/// Rendered SVG.
-#[derive(Debug)]
+/// Parsed SVG, possibly rendered.
 pub struct Svg {
-    pub data: Vec<u8>,
-    pub width: usize,
+    tree: resvg::Tree,
+    data: Vec<u8>,
+    size: u32,
+}
+
+impl fmt::Debug for Svg {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct("Svg").finish()
+    }
 }
 
 impl Svg {
-    /// Render an SVG from a path at a specific size.
-    pub fn from_path<P: AsRef<Path>>(path: P, size: u32) -> Result<Self, Error> {
+    /// Parse a SVG from a path, but don’t render it yet.
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         let file = fs::read(path)?;
-        Self::from_buffer(&file, size)
+        Self::parse(&file)
     }
 
-    /// Render an SVG from XML byte buffer at a specific size.
-    pub fn from_buffer(buffer: &[u8], size: u32) -> Result<Self, Error> {
+    /// Parse a SVG from an XML byte buffer, but don’t render it yet.
+    pub fn parse(buffer: &[u8]) -> Result<Self, Error> {
         let options = Options::default();
         let tree = Tree::from_data(buffer, &options)?;
-        let scale = (size as f32 / tree.size.width()).min(size as f32 / tree.size.height());
-
-        let mut pixmap = Pixmap::new(size, size).ok_or(Error::InvalidSize)?;
         let tree = resvg::Tree::from_usvg(&tree);
-        let transform = Transform::from_scale(scale, scale);
-        tree.render(transform, &mut pixmap.as_mut());
+        let data = Vec::new();
+        let size = 0;
+        Ok(Svg { tree, data, size })
+    }
 
-        let width = pixmap.width() as usize;
-        let data = pixmap.take();
+    /// Render this SVG at a specific size.
+    pub fn render(&mut self, size: u32) -> Result<(&[u8], u32), Error> {
+        if self.size != size {
+            let tree_size = self.tree.size;
+            let scale = (size as f32 / tree_size.width()).min(size as f32 / tree_size.height());
+            let transform = Transform::from_scale(scale, scale);
 
-        Ok(Self { data, width })
+            let mut pixmap = Pixmap::new(size, size).ok_or(Error::InvalidSize)?;
+            self.tree.render(transform, &mut pixmap.as_mut());
+
+            self.size = pixmap.width();
+            self.data = pixmap.take();
+        }
+
+        Ok((&self.data, self.size))
+    }
+
+    pub fn data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn width(&self) -> usize {
+        self.size as usize
     }
 }
